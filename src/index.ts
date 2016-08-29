3bb2970d1cbbcfcad0f88485ad92121a04bd6dce
21stio <ph.woerdehoff+21st@gmail.com>
2016-08-29 17:09:18 +0200

    remove zipkin stuff

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,44 @@
 import * as express from "express";
+import * as swaggerMiddleware from "swagger-express-middleware";
+import * as Knex from "knex";
+import {DependencyResolver} from "./app/DependencyResolver";
 
-var fetch = require('node-fetch');
-const {trace, BatchRecorder, Tracer, ExplicitContext, ConsoleRecorder} = require('zipkin');
-const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
-const {HttpLogger} = require('zipkin-transport-http');
-const CLSContext = require('zipkin-context-cls');
-
-var ctxImpl = new ExplicitContext();
-var recorder = new ConsoleRecorder();
-
-const tracer = new Tracer({
-    recorder: recorder,
-    ctxImpl: new CLSContext('zipkin')
-});
-
-
-//CALLER
-const wrapFetch = require('zipkin-instrumentation-fetch');
-
-const zipkinFetch = wrapFetch(fetch, {
-    tracer,
-    serviceName: process.env.SERVICE_NAME + '-fetch-' + process.env.APPLICATION_PORT,
-    port: process.env.APPLICATION_PORT
-});
-
-if (process.env.FETCH_URL != undefined) {
-    setTimeout(function () {
-        zipkinFetch(process.env.FETCH_URL).then(function (res) {
-            return res.text();
-        }).then(function (body) {
-            console.log(body)
-        });
-    }, 1000)
-}
 
 var app = express();
 
+var knexConfig:Knex.Config = {
+    client: process.env.DATABASE_CLIENT,
+    connection: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_DATABASE,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
+};
 
-//RECEIVER
-app.use(function (request:express.Request, response:express.Response, next) {
-    console.log(request.headers);
+swaggerMiddleware('swagger.yml', app, function (err, middleware) {
+    app.use(
+        middleware.metadata(),
+        middleware.CORS(),
+        middleware.files(),
+        middleware.parseRequest(),
+        middleware.validateRequest()
+    );
 
-    next()
-});
+    var dependencyResolver = new DependencyResolver(knexConfig);
 
-app.use(zipkinMiddleware({
-    tracer,
-    serviceName: process.env.SERVICE_NAME + '-middleware-' + process.env.APPLICATION_PORT,
-    port: process.env.APPLICATION_PORT
-}));
-
-app.use('/', function (request:express.Request, response:express.Response, next) {
-    response.send('Hi');
-});
+    dependencyResolver.getCommunicationResolver().attachRouters(app);
 
 
-
-
-
-app.listen(process.env.APPLICATION_PORT, function () {
-    console.log('The PetStore sample is now running at http://localhost:' + process.env.APPLICATION_PORT);
+    app.listen(process.env.APPLICATION_PORT, function () {
+        console.log('The PetStore sample is now running at http://localhost:' + process.env.APPLICATION_PORT);
+    });
 });
