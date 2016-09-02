b26f6fb07100c1df5782db126850564c9af624c8
21stio <ph.woerdehoff+21st@gmail.com>
2016-09-02 13:45:57 +0200

    compose jenkinsfile

--- gulp/build.js
+++ gulp/build.js
@@ -2,4 +2,4 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('purge_previous_build',  shell.task(['rm -r ./dist/* &>/dev/null']));
-gulp.task('transpile', ['purge_previous_build'], shell.task(['tsc']));
+gulp.task('transpile', shell.task(['tsc']));
