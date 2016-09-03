7c38c59e79ea2c1e1655aa3d6740eda6e76ef96f
21stio <ph.woerdehoff+21st@gmail.com>
2016-09-03 11:56:00 +0200

    compose jenkinsfile

--- src/seeds/pets.ts
+++ src/seeds/pets.ts
@@ -5,9 +5,9 @@ exports.seed = function (knex: knex, Promise) {
     return knex("pets").del()
         .then(function () {
             return Promise.all([
-                knex("pets").insert({id: 1, name: "Walter", tag: "dog"}),
-                knex("pets").insert({id: 2, name: "Mike", tag: "cat"}),
-                knex("pets").insert({id: 3, name: "Frank", tag: "turtle"})
+                knex("pets").insert({name: "Walter", tag: "dog"}),
+                knex("pets").insert({name: "Mike", tag: "cat"}),
+                knex("pets").insert({name: "Frank", tag: "turtle"})
             ]);
         });
 };
